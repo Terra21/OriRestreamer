66195463b1b58569c94181729e4ec03ae533da09
Terra21 <iain.thomson21@gmail.com>
2017-12-11 22:06:35 +0000

    Added magenta boxes for streams

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();  
 var server = require('http').createServer(app);  
 var io = require('socket.io')(server);
+var port = 8080;
 
 app.use(express.static(__dirname + '/node_modules'));  
 app.get('/', function(req, res,next) {  
@@ -18,4 +19,5 @@ io.on('connection', function(client) {
     })
 });
 
-server.listen(8080);  
+server.listen(port);
+console.log('Server listening on port ' + port)
