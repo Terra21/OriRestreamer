3d6652f2727b1ef10e092480bcc87cd726d41f5b
Jeff Hobson <jeffrey296@gmail.com>
2018-03-10 07:16:04 -0500

    Add styling for winners
    Update Match properties
    Styles

--- src/app/services/match.ts
+++ src/app/services/match.ts
@@ -9,12 +9,11 @@ export class Match {
                 player2_result: string = ""){
 		this.name = name;
 		this.round = round;
-        this.player1 = this.getPlayerName(player1);
-        this.player2 = this.getPlayerName(player2);
+        this.player1 = player1;
+        this.player2 = player2;
         this.player1_seed = player1_seed;
         this.player2_seed = player2_seed;
-        this.player1_result = isNaN(parseInt(player1_result)) ? 0 : parseInt(player1_result);
-        this.player2_result = isNaN(parseInt(player2_result)) ? 0 : parseInt(player2_result);
+        this.winner = player1_result === "W" ? player1 : player2_result === "W" ? player2 : "";
     }
 
 	name: string;
@@ -23,20 +22,5 @@ export class Match {
     player2: string;
     player1_seed: string;
     player2_seed: string;
-    player1_result: number;
-    player2_result: number;
-
-    get player1Winner() {
-        return this.player1_result > this.player2_result;
-    }
-
-    get player2Winner(){
-        return this.player2_result > this.player1_result;
-    }
-
-    private getPlayerName(text: string): string {
-        return text;
-        //Used to filter out Winner 5 / Loser 9 ect
-        //return (~ text.indexOf('Winner') || ~ text.indexOf('Loser') ? "" : text)
-    }
+	winner: string;
 }
