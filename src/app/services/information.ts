409de328d8930117f0728fac445a938afab6b4be
Iain Thomson <iain.thomson21@gmail.com>
2018-07-19 22:12:02 +0100

    Add doubles layout core working

--- src/app/services/information.ts
+++ src/app/services/information.ts
@@ -16,7 +16,7 @@ export class Information {
     player1_stats: number = 0;
     player2_stats: number = 0;
     currentAudioOnPlayer: number = 1;
-    commentators: string = 'TheRooseIsLoose, TheRooseIsLoose';
+    commentators: string = 'Sigmasin, JHobz296';
     background: string = 'iceless';
     groupName: string = 'Swiss - Round 2';
     matchType: string = this.groupName + ' (2-0)';
