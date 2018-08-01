59ece31ff52522fb75eb1f3dc7e6f46a57483987
Iain Thomson <iain.thomson21@gmail.com>
2018-08-01 21:31:45 +0100

    Add Basic Site Scraping on Challonge

--- src/app/groups/groups.cmp.ts
+++ src/app/groups/groups.cmp.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnDestroy } from '@angular/core';
+import { Component, Input, OnDestroy, ViewEncapsulation } from '@angular/core';
 import { Observable, Subscription } from 'rxjs';
 import { Information } from '../services/information';
 import { environment } from '../../environments/environment';
@@ -7,27 +7,35 @@ import * as $ from 'jquery';
 import { Socket } from 'net';
 
 @Component({
-  templateUrl: './groups.html',
-  styleUrls: ['./groups.css']
+	templateUrl: './groups.html',
+	encapsulation: ViewEncapsulation.None,
+	styleUrls: ['./groups.css']
 })
 
 export class GroupsCMP {
-  constructor() { }
+  	constructor() { }
 
-  ngOnInit(){
-    this.socket.on('data', function(data: Information){
+ 	 ngOnInit(){
+		$.getJSON('http://www.whateverorigin.org/get?url=' + encodeURIComponent('https://challonge.com/orirando2018singlesswiss') + '&callback=?', function(data){
+			let webpage = data.contents;
+			let swissTable = $(webpage).find('#scorecard');
+			
+			$('#standings').append(swissTable);
+		});
+
+    	this.socket.on('data', function(data: Information){
       
-      if(data.seed !== this.seed)
-        return;
+      	if(data.seed !== this.seed)
+        	return;
 
-      
 
-    }.bind(this));
-  }
 
-  public vm: Information = new Information();
+    	}.bind(this));
+  	}
 
-  swiss: any;
-  socket: any = io.connect(environment.socketPath);
-  seed: string = window.location.href.split('=')[1];
+  	public vm: Information = new Information();
+
+  	swiss: any;
+  	socket: any = io.connect(environment.socketPath);
+  	seed: string = window.location.href.split('=')[1];
 }
